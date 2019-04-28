f4dead879abe7ed0453df42aa2c94a852782627f
Liam <lon.liam.li@gmail.com>
2019-04-28 11:36:27 -0700

    update

---
+++ src/app/components/hosting/hosting.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hosting',
+  templateUrl: './hosting.component.html',
+  styleUrls: ['./hosting.component.css']
+})
+export class HostingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
