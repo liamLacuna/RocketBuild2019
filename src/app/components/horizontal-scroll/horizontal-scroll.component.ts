617386b361fd09634a7bacdf50dbfb66ab28a352
Richard Mac <richard.mac2244@gmail.com>
2019-04-27 18:25:01 -0700

    richard update

---
+++ src/app/components/horizontal-scroll/horizontal-scroll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-horizontal-scroll',
+  templateUrl: './horizontal-scroll.component.html',
+  styleUrls: ['./horizontal-scroll.component.css']
+})
+export class HorizontalScrollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
